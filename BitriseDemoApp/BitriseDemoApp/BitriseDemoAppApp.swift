f5c7b831557df708b6c24e7b0249d0952363363f
btxn-ozguataseven <ozgu.ataseven@bitexen.com>
2024-02-14 13:06:08 +0300

    first commit

---
+++ BitriseDemoApp/BitriseDemoApp/BitriseDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitriseDemoAppApp.swift
+//  BitriseDemoApp
+//
+//  Created by Özgü Ataseven on 14.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BitriseDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
